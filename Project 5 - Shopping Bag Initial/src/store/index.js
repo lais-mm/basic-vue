687f02ce2b63fc4b257cbf3a728ce3c5000670f8
Laís Moreira <lais-mm@outlook.com>
2024-04-16 20:51:00 -0300

    att products

--- Project 5 - Shopping Bag Initial/src/store/index.js
+++ Project 5 - Shopping Bag Initial/src/store/index.js
@@ -3,13 +3,17 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    products: []
+    products: [],
+    productsInBag: []
   },
-  
+
   mutations: {
 
     loadProducts (state, products){
       state.products = products;
+    },
+    addToBag (state, product) {
+      state.productsInBag.push(product);
     }
   },
 
@@ -21,6 +25,11 @@ export default createStore({
       then(response => {
         commit('loadProducts', response.data)
       })
+    },
+
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+
     }
   },
 
